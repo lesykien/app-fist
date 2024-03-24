89c122ed22ee459ddba4d9b4c1dbf8dd1a956e8a
lesykien <lesykien030@gmail.com>
2024-03-24 20:28:24 +0700

    Hoàn thiện kiểm lỗi form

--- User/components/checkout/CheckController.js
+++ User/components/checkout/CheckController.js
@@ -36,11 +36,20 @@ app.controller('CheckController', function ($scope, $http, $rootScope, $location
         console.log(JSON.parse(localStorage.getItem(idAccount)));
     }
 
+    // Hàm xử lý thanh toán trực tuyến
+    $scope.Banking = function () {
+        if ($scope.FormBanking.$valid) {
+            console.log(2);
+        }
+        else {
+            alert('Bạn chưa nhập thông tin nhận hàng')
+        }
+    }
+
     // Thanh toán khi nhận hàng
-    $scope.CheckOut = function(){
+    $scope.CheckOut = function () {
         $scope.btn_ordering = true
-        console.log(2);
-    } 
+    }
     // chép thông tin sản phẩm vào mảng chỉ lấy id và số lượng mua
     var ListCart = []
     for (var i = 0; i < $scope.Cart.length; i++) {
@@ -50,40 +59,46 @@ app.controller('CheckController', function ($scope, $http, $rootScope, $location
         }
         ListCart.push(New)
     }
+    // hàm xử lý thanh toán khi nhận hàng
     $scope.Ordering = function () {
-        // chép thông tin giao hàng đến localStorage khác
-        var informationDelivery = {
-            FullName: Acc.fullName,
-            Address: $scope.Address,
-            Email: Acc.email,
-            Phone: $scope.Phone,
-            Note: $scope.Note,
-        }
-        localStorage.setItem(Acc.id, JSON.stringify(informationDelivery))
-        var orderDto = {
-            idAccount: Acc.id,
-            totalAmount: $scope.Total,
-            dateTime: "2024-03-18T07:49:03.262Z",
-            statusPayment: true,
-            statusOrder: 0,
-            statusDelivery: 1,
-            methodPayment: 1,
-            orderItems: ListCart
-        }
-        // Gửi thông tin về đơn hàng về api
-        $http.post('https://localhost:7272/*api/Orders/orders', orderDto, {
-            headers: { 'Content-Type': 'application/json' }
-        })
-            .then(function (response) {
-                alert('Đặt hàng thành công!!!')
-                localStorage.removeItem('ListProduct')
-                $location.path('/Account')
-                $rootScope.CountCart = 0 ;
+        if ($scope.FormBanking.$valid) {
+            // chép thông tin giao hàng đến localStorage khác
+            var informationDelivery = {
+                FullName: Acc.fullName,
+                Address: $scope.Address,
+                Email: Acc.email,
+                Phone: $scope.Phone,
+                Note: $scope.Note,
+            }
+            localStorage.setItem(Acc.id, JSON.stringify(informationDelivery))
+            var orderDto = {
+                idAccount: Acc.id,
+                totalAmount: $scope.Total,
+                dateTime: "2024-03-18T07:49:03.262Z",
+                statusPayment: true,
+                statusOrder: 0,
+                statusDelivery: 1,
+                methodPayment: 1,
+                orderItems: ListCart
+            }
+            // Gửi thông tin về đơn hàng về api
+            $http.post('https://localhost:7272/*api/Orders/orders', orderDto, {
+                headers: { 'Content-Type': 'application/json' }
             })
-            .catch(function (error) {
-                alert('Không thể thêm được sản phẩm')
-            });
+                .then(function (response) {
+                    alert('Đặt hàng thành công!!!')
+                    localStorage.removeItem('ListProduct')
+                    $location.path('/Account')
+                    $rootScope.CountCart = 0;
+                })
+                .catch(function (error) {
+                    alert('Không thể thêm được sản phẩm')
+                });
 
+        }
+        else {
+            alert('Bạn chưa nhập thông tin nhận hàng')
+        }
     }
 
 })
