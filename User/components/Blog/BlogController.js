f26ad056c1588d3d7d84defb52dc2eaac63ec2f3
lesykien <lesykien030@gmail.com>
2024-03-25 21:05:11 +0700

    complete get value of blog

--- User/components/Blog/BlogController.js
+++ User/components/Blog/BlogController.js
@@ -1,21 +1,36 @@
-app.controller('BlogController' , function($scope , $rootScope, $http,$sce){
-    $rootScope.siliderShow = false;
-    let ListBlogCart = []
-    $http.get(`https://localhost:7156/api/Blog/get-value`)
+app.controller('BlogController', function ($scope, $rootScope, $http, $sce, $routeParams) {
+  $rootScope.siliderShow = false;
+  // get information blog realte to
+  let ListBlogCart = []
+  $http.get(`https://localhost:7156/api/Blog/get-value`)
     .then(function (response) {
-        for (let i = 0; i < response.data.length; i++) {
-          const object = {
-            contenet : $sce.trustAsHtml(response.data[i].contenet),
-            datePush : moment(response.data[i].datePush).format('DD/MM/YYYY'),
-            image : response.data[i].image, 
-            healine : response.data[i].healine,
-            id : response.data[i].id
-          }
-          ListBlogCart.push(object);
+      for (let i = 0; i < response.data.length; i++) {
+        const object = {
+          contenet: $sce.trustAsHtml(response.data[i].contenet),
+          datePush: moment(response.data[i].datePush).format('DD/MM/YYYY'),
+          image: response.data[i].image,
+          healine: response.data[i].healine,
+          id: response.data[i].id
         }
-        $scope.ListBlog  = ListBlogCart
+        ListBlogCart.push(object);
+      }
+      $scope.ListBlog = ListBlogCart
     })
-    const informaiton = localStorage.getItem('item');
-    const item = document.getElementById('item'); 
-    item.innerHTML = informaiton
+
+  // get inforamtion blof follow id
+  const item = document.getElementById('item');
+  $http.get(`https://localhost:7156/api/Blog/get-id/=${$routeParams.id}`)
+    .then(function (response) {
+      const object = {
+        contenet: $sce.trustAsHtml(response.data.contenet),
+        datePush: moment(response.data.datePush).format('DD/MM/YYYY HH:MM'),
+        image: response.data.image,
+        healine: response.data.healine,
+        id: response.data.id
+      }
+      $scope.Inforamtion = object
+      // check content enter document item 
+      item.innerHTML = object.contenet
+    })
+
 });
