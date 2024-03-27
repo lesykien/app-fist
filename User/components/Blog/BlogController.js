b83b920506e2d96848e420d9a6c0b190d938df86
lesykien <lesykien030@gmail.com>
2024-03-27 21:07:22 +0700

    convert item length blog in blog detal

--- User/components/Blog/BlogController.js
+++ User/components/Blog/BlogController.js
@@ -4,7 +4,7 @@ app.controller('BlogController', function ($scope, $rootScope, $http, $sce, $rou
   let ListBlogCart = []
   $http.get(`https://localhost:7156/api/Blog/get-value`)
     .then(function (response) {
-      for (let i = 0; i < response.data.length; i++) {
+      for (let i = 0; i < 4; i++) {
         const object = {
           contenet: $sce.trustAsHtml(response.data[i].contenet),
           datePush: moment(response.data[i].datePush).format('DD/MM/YYYY'),
